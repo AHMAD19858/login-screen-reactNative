54c9b2f5e43344bf52750155bf1cb05da43dea23
AHMAD19858 <61994278+AHMAD19858@users.noreply.github.com>
2020-10-05 23:19:02 +0200

    Animated login screen

--- App.js
+++ App.js
@@ -1,14 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {AppLoading} from 'expo';
+import { Asset} from 'expo-asset';
+import MusicApp from './screens/index';
+function cacheImage (images){
+  return images.map(image =>{
+    if(typeof image == 'string'){
+      return image.prefetch(image);
+    }else{
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+export default class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      isReady:false,
+    }
+  }
 
-export default function App() {
+  async _loadAssetsAsync(){
+    const imageAssets = cacheImage([require('./assets/bg.jpg') ])
+      await Promise.all([...imageAssets])
+   
+  }
+  render(){
+    if(!this.state.isReady){
+      return(
+        <AppLoading
+        startAsync={this._loadAssetsAsync}
+        onFinish={()=>this.setState({isReady:true})}
+        onError={console.warn}
+        />
+      )
+    }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <MusicApp/>
   );
+}
 }
 
 const styles = StyleSheet.create({
